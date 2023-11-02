53ebe4414c120a17b2fcc3d6c85c196eb33de545
EdisonP <133362311+edions@users.noreply.github.com>
2023-11-02 19:20:20 +0800

    Add design and layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import '@picocss/pico'
+</script>
+
+<div class="container">
+	<hgroup>
+		<h1>
+			<a href="/"> SvelteKit Demo</a>
+		</h1>
+
+		<h2>Simple CRUD application.</h2>
+	</hgroup>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid">
+    <!-- <div>
+        <h2>Article:</h2>
+        <article>
+            <header>Example Article</header>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis nobis mollitia veritatis ea officia accusamus aliquid molestiae exercitationem reprehenderit quod, saepe aliquam quidem vero est sapiente facilis. Dolorem, delectus quas?</p>
+            <button class="outline secondary">Delete Article</button>
+            <a href="/" role="button" class="outline contrast" style="width: 100%;">Edit Article</a>
+        </article>
+    </div> -->
+    <form action="" method="POST">
+        <h1>New Article</h1>
+        <label for="title">Title</label>
+        <input type="text" id="title" name="title">
+        <label for="title">title</label>
+        <textarea name="content" id="content" rows={5} />
+        <button type="submit">Add Article</button>
+    </form>
+</div>
